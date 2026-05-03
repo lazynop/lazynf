a60d2da3470d74e4dedf240957828096bad3e03c
Steven Raimondi <traveltoaiur@gmail.com>
2026-05-03 16:30:07 +0200

    feat(cmd): search command

---
+++ cmd/search.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"github.com/lazynop/vellum/internal/fonts"
+	"github.com/lazynop/vellum/internal/github"
+	"github.com/lazynop/vellum/internal/xdg"
+	"github.com/spf13/cobra"
+)
+
+func newSearchCmd() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "search <query>",
+		Short: "Search the catalog by case-insensitive substring",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(_ *cobra.Command, args []string) error {
+			v := Verbosity()
+			gh := github.NewClient()
+			v.Debugf("github auth source: %s", gh.AuthSource())
+
+			cat, err := fonts.ResolveCatalog(gh, xdg.CatalogFile())
+			if err != nil {
+				return err
+			}
+			for _, n := range fonts.Search(cat.Fonts, args[0]) {
+				v.Info("%s", n)
+			}
+			return nil
+		},
+	}
+	return c
+}
--- cmd/stubs.go
+++ cmd/stubs.go
@@ -2,7 +2,6 @@ package cmd
 
 import "github.com/spf13/cobra"
 
-func newSearchCmd() *cobra.Command { return &cobra.Command{Use: "search", Hidden: true, RunE: stubRun} }
-func newCacheCmd() *cobra.Command  { return &cobra.Command{Use: "cache", Hidden: true, RunE: stubRun} }
+func newCacheCmd() *cobra.Command { return &cobra.Command{Use: "cache", Hidden: true, RunE: stubRun} }
 
 func stubRun(_ *cobra.Command, _ []string) error { return nil }
